25428dd2faac10892fc798f23ee97bc085481c7c
ElizaPeterson <kuipercat@gmail.com>
2023-01-18 20:24:56 +0000

    first commit

---
+++ tests/customer_test.py
@@ -0,0 +1,19 @@
+import unittest
+from src.customer import Customer
+from src.drink import Drink
+
+
+class TestCustomer(unittest.TestCase):
+    def setUp(self):
+        self.customer = Customer("Rupert", 10, 17)
+        self.drink = Drink("Tennents", 2.50, 2)
+
+    def test_buy_a_drink(self):
+        self.customer.buy_a_drink(2.50)
+        self.assertEqual(7.50, self.customer.wallet)
+
+    # def test_increase_drunkenness(self):
+    #     self.customer.increase_drunkenness(self.drink)
+    #     self.assertEqual(2, self.customer.increase_drunkenness(self.drink))
+    
+    # This is where I ended, I probably could have finished, but want to get a good sleep!
